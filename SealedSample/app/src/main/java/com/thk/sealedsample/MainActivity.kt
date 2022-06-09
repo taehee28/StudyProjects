882d21e377566a6e911be94df6643263a9794454
Kang Taehee <dev.thk28@gmail.com>
2022-06-09 17:02:53 +0900

    add: Hilt 라이브러리 추가 및 적용

--- SealedSample/app/src/main/java/com/thk/sealedsample/MainActivity.kt
+++ SealedSample/app/src/main/java/com/thk/sealedsample/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.thk.sealedsample.ui.theme.SealedSampleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val TAG = MainActivity::class.simpleName
 
---
+++ SealedSample/app/src/main/java/com/thk/sealedsample/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.thk.sealedsample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
