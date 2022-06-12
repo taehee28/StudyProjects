46aea6bf0a1f36c763e555eb6151fdea4c8c3e50
taehee28 <dev.thk28@gmail.com>
2022-06-12 21:55:49 +0900

    refactor: Composable 수정
    * UI 수정
    * Composable 안의 when에서 is로 smart cast 안되는 부분 수정

--- SealedSample/app/src/main/java/com/thk/sealedsample/MainActivity.kt
+++ SealedSample/app/src/main/java/com/thk/sealedsample/MainActivity.kt
@@ -5,16 +5,14 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,20 +50,39 @@ class MainActivity : ComponentActivity() {
 fun StateView(uiStateFlow: StateFlow<UiState<List<Post>>>) {
     val uiState by uiStateFlow.collectAsState()
 
-    when (uiState) {
-        is UiState.Init -> {
-            CircularProgressIndicator(
-                modifier = Modifier.width(50.dp).heightIn(50.dp),
-                strokeWidth = 10.dp
-            )
-        }
-        is UiState.Success -> {
-            Text(text = uiState.toString())
-        }
-        is UiState.Error -> {
-            Text(text = uiState.toString())
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        when (val state = uiState) {
+            is UiState.Init -> {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .heightIn(100.dp),
+                    strokeWidth = 5.dp
+                )
+            }
+            is UiState.Success -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(state.data) {
+                        Text(
+                            text = it.toString(),
+                            modifier = Modifier.padding(8.dp)
+                        )
+                        Divider()
+                    }
+                }
+            }
+            is UiState.Error -> {
+                Text(
+                    text = state.message ?: "null"
+                )
+            }
         }
     }
+
 }
 
 @Composable
