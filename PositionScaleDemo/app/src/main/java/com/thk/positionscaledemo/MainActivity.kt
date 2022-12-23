24a3db53d2e64578486bd5631f4c5c5f07db6a32
Kang Taehee <dev.thk28@gmail.com>
2022-12-23 15:08:10 +0900

    test code 작성

--- PositionScaleDemo/app/src/main/java/com/thk/positionscaledemo/MainActivity.kt
+++ PositionScaleDemo/app/src/main/java/com/thk/positionscaledemo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.thk.positionscaledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.thk.positionscaledemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = MainActivity::class.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val xRevision = resources.getDimension(R.dimen.map_width) / 520
+        val yRevision = resources.getDimension(R.dimen.map_height) / 580
+
+        val view = View(this).apply {
+            layoutParams = FrameLayout.LayoutParams(resources.getDimensionPixelSize(R.dimen.icon_width), resources.getDimensionPixelSize(R.dimen.icon_height))
+            setBackgroundResource(R.color.purple_700)
+            x = 315f * xRevision
+            y = 220f * yRevision
+        }
+
+        binding.frameLayout.addView(view)
     }
 }
---
+++ PositionScaleDemo/app/src/main/java/com/thk/positionscaledemo/PositionScaleDemoApplication.kt
@@ -0,0 +1,7 @@
+package com.thk.positionscaledemo
+
+import android.app.Application
+import com.bumptech.glide.annotation.GlideModule
+
+@GlideModule
+class PositionScaleDemoApplication : Application()
