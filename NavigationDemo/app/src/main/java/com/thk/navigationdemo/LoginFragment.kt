31d5b5ea789cf35b4f57f84fa360b134546e1183
taehee28 <dev.thk28@gmail.com>
2023-05-22 21:17:25 +0900

    MainActivity에 Navigation 적용
    * root_nav_graph 정의
    * MainAcivity에 fragment 표시할 FragmentContainerView 추가

--- NavigationDemo/app/src/main/java/com/thk/navigationdemo/LoginFragment.kt
+++ NavigationDemo/app/src/main/java/com/thk/navigationdemo/LoginFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.thk.navigationdemo.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding
+        get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
